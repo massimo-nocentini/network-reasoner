3a9b639b81e27a0078e1d5948b3f9e683ff0449a
Massimo Nocentini <massimo.nocentini@gmail.com>
2013-07-20 14:48:01 +0200

    Renamed the location to Math.net library and create a project for learning tests on this library.

---
+++ it.unifi.dsi.stlab.math.algebra.tests/Test.cs
@@ -0,0 +1,15 @@
+using System;
+using NUnit.Framework;
+
+namespace it.unifi.dsi.stlab.math.algebra.tests
+{
+	[TestFixture()]
+	public class Test
+	{
+		[Test()]
+		public void TestCase ()
+		{
+		}
+	}
+}
+
---
+++ third-party-libraries/MathNet.Numerics/README.txt
@@ -0,0 +1,1 @@
+The version of the library is MathNet.Numerics-2.5.0.27
