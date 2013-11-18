6c952fd39af42ffed57f214f7bda59a220d6af5e
Massimo Nocentini <massimo.nocentini@gmail.com>
2013-11-18 08:00:01 +0100

    Add new tests for the other possible combinations for composing the wrapping structure.

--- it.unifi.dsi.stlab.recursion/Testing.cs
+++ it.unifi.dsi.stlab.recursion/Testing.cs
@@ -28,7 +28,7 @@ namespace it.unifi.dsi.stlab.recursion
 		}
 		
 		[Test()]
-		public void tying_recursion_on_foreing_object ()
+		public void tying_recursion_on_foreing_do_something_decorator_object ()
 		{
 			var baseObjectMaker = new SelfRecursion<AtomicConcept, AbstractBase> (
 				new Func<AtomicConcept> (() => new AtomicConcept ()));
@@ -41,17 +41,63 @@ namespace it.unifi.dsi.stlab.recursion
 			AbstractBase decorator = foreingSelfRecurionMaker.makeInstance ();
 
 			Assert.That (baseObject.Self, Is.SameAs (decorator));
+		
+			StringBuilder accumulator = new StringBuilder ();
+			decorator.doSomething (accumulator);
+			StringBuilder expected = new StringBuilder ();
+			expected.AppendLine ("DoSomethingDecorator: PRE decoration for do something method");
+			expected.AppendLine ("AtomicConcept: before doing something we need to do something else.");
+			expected.AppendLine ("AtomicConcept: we're doing something else now.");
+			expected.AppendLine ("AtomicConcept: now that we did something else we can do something.");
+			expected.AppendLine ("DoSomethingDecorator: POST decoration for do something method");
+
+			Assert.That (accumulator.ToString (), Is.EqualTo (expected.ToString ()));
+		
 		}
 
 		[Test()]
-		public void tying_recursion_on_foreing_object_of_order_two ()
+		public void tying_recursion_on_foreing_do_something_else_decorator_object ()
 		{
 			var baseObjectMaker = new SelfRecursion<AtomicConcept, AbstractBase> (
 				new Func<AtomicConcept> (() => new AtomicConcept ()));
 
 			AbstractBase baseObject = baseObjectMaker.makeInstance ();
 
-			AbstractBase doSomethingDecorator = DoSomethingDecorator.ctor (baseObject).Invoke ();
+			var foreingSelfRecurionMaker = new ForeignRecursion<DoSomethingElseDecorator, AbstractBase> (
+				baseObject, DoSomethingElseDecorator.ctor (baseObject));
+
+			AbstractBase decorator = foreingSelfRecurionMaker.makeInstance ();
+
+			Assert.That (baseObject.Self, Is.SameAs (decorator));
+		
+			StringBuilder accumulator = new StringBuilder ();
+			decorator.doSomething (accumulator);
+			StringBuilder expected = new StringBuilder ();
+
+			expected.AppendLine ("AtomicConcept: before doing something we need to do something else.");
+			expected.AppendLine ("DoSomethingElseDecorator: PRE decoration for do something else method");
+			expected.AppendLine ("AtomicConcept: we're doing something else now.");
+			expected.AppendLine ("DoSomethingElseDecorator: POST decoration for do something else method");
+			expected.AppendLine ("AtomicConcept: now that we did something else we can do something.");
+
+			Assert.That (accumulator.ToString (), Is.EqualTo (expected.ToString ()));
+		
+		}
+
+		[Test()]
+		public void tying_recursion_on_foreing_object_do_something_else_decore_do_something_decore_base ()
+		{
+			var baseObjectMaker = new SelfRecursion<AtomicConcept, AbstractBase> (
+				new Func<AtomicConcept> (() => new AtomicConcept ()));
+
+			AbstractBase baseObject = baseObjectMaker.makeInstance ();
+
+//			AbstractBase doSomethingDecorator = DoSomethingDecorator.ctor (baseObject).Invoke ();
+
+			var doSomethingRecurionMaker = new ForeignRecursion<DoSomethingDecorator, AbstractBase> (
+				baseObject, DoSomethingDecorator.ctor (baseObject));
+
+			var doSomethingDecorator = doSomethingRecurionMaker.makeInstance ();
 
 			var doSomethingElseRecurionMaker = new ForeignRecursion<DoSomethingElseDecorator, AbstractBase> (
 				baseObject, DoSomethingElseDecorator.ctor (doSomethingDecorator));
@@ -59,6 +105,22 @@ namespace it.unifi.dsi.stlab.recursion
 			var doSomethingElseDecorator = doSomethingElseRecurionMaker.makeInstance ();
 
 			Assert.That (baseObject.Self, Is.SameAs (doSomethingElseDecorator));
+			Assert.That (doSomethingDecorator.Self, Is.Null);
+			
+			StringBuilder accumulator = new StringBuilder ();
+			doSomethingElseDecorator.doSomething (accumulator);
+
+			StringBuilder expected = new StringBuilder ();
+			expected.AppendLine ("DoSomethingDecorator: PRE decoration for do something method");
+			expected.AppendLine ("AtomicConcept: before doing something we need to do something else.");
+			expected.AppendLine ("DoSomethingElseDecorator: PRE decoration for do something else method");
+			expected.AppendLine ("AtomicConcept: we're doing something else now.");
+			expected.AppendLine ("DoSomethingElseDecorator: POST decoration for do something else method");
+			expected.AppendLine ("AtomicConcept: now that we did something else we can do something.");
+			expected.AppendLine ("DoSomethingDecorator: POST decoration for do something method");
+			
+			Assert.That (accumulator.ToString (), Is.EqualTo (expected.ToString ()));
+		
 		}
 	}
 }
