9c38774795cb8fc267c4fbd2a10044c7d51918a2
Massimo Nocentini <massimo.nocentini@gmail.com>
2014-05-27 13:28:00 +0200

    Little refactoring on names and types declaration for water specialized visitor.

--- it.unifi.dsi.stlab.networkreasoner.gas.system.formulae/visitors/GasFormulaVisitorExactlyDimensionedForWater.cs
+++ it.unifi.dsi.stlab.networkreasoner.gas.system.formulae/visitors/GasFormulaVisitorExactlyDimensionedForWater.cs
@@ -7,21 +7,21 @@ namespace it.unifi.dsi.stlab.networkreasoner.gas.system.formulae
 		public override double visitCoefficientFormulaForNodeWithSupplyGadget (
 			CoefficientFormulaForNodeWithSupplyGadget aSupplyNodeFormula)
 		{
-			var AirPressureInBar = this.computeAirPressureFromHeightHolder (
-				aSupplyNodeFormula);
-
-			var specWeight = AmbientParameters.RefDensity () * 
-				AmbientParameters.GravitationalAcceleration;
-
-			// pressione assoluta al nodo espressa in metri
-			var numerator = (AirPressureInBar + 
-				aSupplyNodeFormula.GadgetSetupPressureInMillibar) * 1e5 // per andare in pascal
-				/ specWeight;
+//			var AirPressureInBar = this.computeAirPressureFromHeightHolder (
+//				                       aSupplyNodeFormula);
+//
+//			var specWeight = AmbientParameters.RefDensity () *
+//			                 AmbientParameters.GravitationalAcceleration;
+//
+//			// pressione assoluta al nodo espressa in metri
+//			var numerator = (AirPressureInBar +
+//			                aSupplyNodeFormula.GadgetSetupPressureInMillibar) * 1e5// per andare in pascal
+//			                / specWeight;
 
 //			var result = numerator + aSupplyNodeFormula.NodeHeight;
 		
-			var result = aSupplyNodeFormula.GadgetSetupPressureInMillibar + 
-				aSupplyNodeFormula.NodeHeight;
+			var result = aSupplyNodeFormula.GadgetSetupPressureInMillibar +
+			             aSupplyNodeFormula.NodeHeight;
 
 			return result;
 		}
@@ -29,32 +29,31 @@ namespace it.unifi.dsi.stlab.networkreasoner.gas.system.formulae
 		public override double visitRelativePressureFromAdimensionalPressureFormulaForNodes (
 			RelativePressureFromAdimensionalPressureFormulaForNodes aRelativePressureFromAbsolutePressureFormula)
 		{
-			var AirPressureInBar = this.computeAirPressureFromHeightHolder (
-				aRelativePressureFromAbsolutePressureFormula);
-
-			var z = aRelativePressureFromAbsolutePressureFormula.AbsolutePressure;
-			var h = aRelativePressureFromAbsolutePressureFormula.NodeHeight;	
-					
-			var specWeight = AmbientParameters.RefDensity () * 
-				AmbientParameters.GravitationalAcceleration;
+//			var AirPressureInBar = this.computeAirPressureFromHeightHolder (
+//				                       aRelativePressureFromAbsolutePressureFormula);
+//
+//			var specWeight = AmbientParameters.RefDensity () *
+//				AmbientParameters.GravitationalAcceleration;
+//
 
 //			var result = (z - h) * specWeight * 1e-5;
-			var result = (z - h);
 
-//			return (result - AirPressureInBar);
+			double result = aRelativePressureFromAbsolutePressureFormula.AbsolutePressure -
+			                aRelativePressureFromAbsolutePressureFormula.NodeHeight;
+
 			return result;
 		}
 
 		public override double visitCovariantLittleKFormula (
 			CovariantLittleKFormula covariantLittleKFormula)
 		{
-			return 1;
+			return 1d;
 		}
 
 		public override double visitControVariantLittleKFormula (
 			ControVariantLittleKFormula controVariantLittleKFormula)
 		{
-			return 1;
+			return 1d;
 		}
 
 		public override double visitVelocityValueFormula (VelocityValueFormula velocityValueFormula)
@@ -64,8 +63,8 @@ namespace it.unifi.dsi.stlab.networkreasoner.gas.system.formulae
 			double absolutePressureOfStartNode = velocityValueFormula.AbsolutePressureOfStartNode;
 			double absolutePressureOfEndNode = velocityValueFormula.AbsolutePressureOfEndNode;
 		
-			var numerator = Qvalue / 3600d;
-			var denominator = Math.PI * Math.Pow (diameter * 1e-3, 2d) / 4d; 
+			double numerator = Qvalue / 3600d;
+			double denominator = Math.PI * Math.Pow (diameter * 1e-3, 2d) / 4d; 
 
 			return numerator / denominator;
 			
