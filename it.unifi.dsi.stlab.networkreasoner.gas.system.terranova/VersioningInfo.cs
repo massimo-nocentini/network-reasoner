54612b02ca25b9df540960acfc7db0b5212a12f1
Massimo Nocentini <massimo.nocentini@gmail.com>
2014-11-18 14:34:40 +0100

    Advance version number scheme to include pressure regulator gadget.

--- it.unifi.dsi.stlab.networkreasoner.gas.system.terranova/VersioningInfo.cs
+++ it.unifi.dsi.stlab.networkreasoner.gas.system.terranova/VersioningInfo.cs
@@ -4,9 +4,24 @@ namespace it.unifi.dsi.stlab.networkreasoner.gas.system.terranova
 {
 	public class VersioningInfo
 	{
+		/// <summary>
+		/// Gets the version number.
+		/// 
+		/// Here are the stack of versions with a brief description:
+		/// 
+		/// v1.1.0: introduce pressure regulator gadget for edges.
+		/// 
+		/// v1.0.1: enhancement to tackle turbolent behavior due to 
+		/// 		Reynolds number.
+		/// 
+		/// v1.0.0: basic version with checker about negative pressures
+		/// 		associated to nodes with load gadget.
+		/// 
+		/// </summary>
+		/// <value>The version number.</value>
 		public String VersionNumber { 
 			get {
-				return "v1.0.1";
+				return "v1.1.0";
 			}
 		}
 
